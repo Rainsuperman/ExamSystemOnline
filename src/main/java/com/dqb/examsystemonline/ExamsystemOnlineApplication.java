8576faad15bf44d3d337310a3258154ce985cd4f
邓庆博 <qingbo.deng@hand-china.com>
2018-08-22 10:17:25 +0800

    添加项目

---
+++ src/main/java/com/dqb/examsystemonline/ExamsystemOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.dqb.examsystemonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsystemOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamsystemOnlineApplication.class, args);
+    }
+}
